06dea3db495f813d2f1c6c1b1453075b8594712b
BLUENOTION <107662470+NYT22@users.noreply.github.com>
2025-02-08 05:53:54 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VETERANOS DEL PANICO</title>
+<link rel="icon" href="png vdp.png">
+<link rel="stylesheet" href="styles.css">
+
+
+<style>
+  /* Estilos para el menú */
+  nav {
+    background-color: black;
+    overflow: hidden;
+  }
+  
+  nav a {
+    float: left;
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  
+  nav a:hover {
+    background-color: #ddd;
+    color: black;
+  }
+  
+  /* Estilos para el contenido desplegable */
+  .dropdown {
+    float: left;
+    overflow: hidden;
+  }
+  
+  .dropdown .dropbtn {
+    font-size: 16px;  
+    border: none;
+    outline: none;
+    color: white;
+    padding: 14px 16px;
+    background-color: inherit;
+    font-family: inherit;
+    margin: 0;
+  }
+  
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.0);
+    z-index: 1;
+  }
+  
+  .dropdown-content a {
+    float: none;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+  }
+  
+  .dropdown-content a:hover {
+    background-color: #ddd;
+  }
+  
+  .dropdown:hover .dropdown-content {
+    display: block;
+  }
+  </style>
+
+</head>
+<body>
+  <nav>
+    <a href="#">Inicio</a>
+ 
+
+
+  
+  <header>
+    <div class="container">
+        <p class="logo"><img src="logo vdp.png" alt=""></p>
+        <h2 class="logo">veteranos del panico</h2>
+    </div>
+  </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div id="contenedor-video">
+        <script src="https://fast.wistia.com/player.js" async></script>
+        <script src="https://fast.wistia.com/embed/xkv5w0yso2.js" async type="module"></script>
+        <style>
+          wistia-player[media-id='xkv5w0yso2']:not(:defined) {
+            background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/xkv5w0yso2/swatch');
+            display: block;
+            filter: blur(5px);
+          }
+        </style>
+        <wistia-player media-id="xkv5w0yso2" aspect="1.7777777777777777" style="width: 889px; height: 500px;"></wistia-player>
+      </div>
+
+      <div id="contenedor-video">
+        <script src="https://fast.wistia.com/player.js" async></script>
+        <script src="https://fast.wistia.com/embed/zy5s64d0io.js" async type="module"></script>
+        <style>
+          wistia-player[media-id='zy5s64d0io']:not(:defined) {
+            background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/zy5s64d0io/swatch');
+            display: block;
+            filter: blur(5px);
+          }
+        </style>
+        <wistia-player media-id="zy5s64d0io" aspect="1.7777777777777777" style="width: 889px;height: 500px;"></wistia-player>
+      </div>
+
+
+      <p>
+        hola, somos veteranos del panico, una banda que se formo a principios del 2024 y fines del 2023, nosotros nomas queremos tocar la musica que nos gusta y divertirnos.
+        por ahora no tenemos nada en esta pagina pero con el tiempo la vamos a ir actualizando
+      </p>
+        <p></p>
+
+
+
+</body>
+</html>
